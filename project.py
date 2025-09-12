898be820c0b2ee97c8a3eb5951a8cf4e6a8e1fbb
Jack Wright <jackwright241604@gmail.com>
2025-09-12 16:50:24 +0100

    Update project.py
    Improved type hints.

--- project.py
+++ project.py
@@ -19,31 +19,31 @@ def main() -> None:
             "Or Research The Properties Of Individual Residues Via The Residue Analyser.\n\n"
             "Disclaimer: Hydrophobicity Scores Do Not Consider The Effects Of Protein Folding."
         )  # Displays instructions.
-        user_choice = input(
-            "\nType '1' For Peptide Analyser. Type '2' For Residue Analyser: "
+        user_choice: str = str(
+            input("\nType '1' For Peptide Analyser. Type '2' For Residue Analyser: ")
         )  # Get choice.
         while user_choice != "1" and user_choice != "2":
             print(
                 "\nPlease Choose Option '1' Or '2'."
             )  # Prompts user until valid choice.
-            user_choice = input(
-                "\nType '1' For Peptide Analyser. Type '2' For Residue Analyser: "
+            user_choice = str(
+                input("\nType '1' For Peptide Analyser. Type '2' For Residue Analyser: ")
             )  # Get choice.
         if user_choice == "1":  # Peptide chain analyser route.
-            raw_chain = (
+            raw_chain: str = str(
                 input("\nInput Chain: ").strip().upper()
             )  # Gets peptide chain input.
-            chain = peptide_check(raw_chain)  # Validates input.
-            molecular_weight = calculate_molecular_weight(
+            chain: str = peptide_check(raw_chain)  # Validates input.
+            molecular_weight: float = calculate_molecular_weight(
                 chain
             )  # Calculates molecular weight.
-            hydrophobicity = calculate_hydrophobicity(
+            hydrophobicity: float = calculate_hydrophobicity(
                 chain
             )  # Calculates hydrophobicity.
-            percentage_composition = calculate_percentage_composition(
+            percentage_composition: str = calculate_percentage_composition(
                 chain
             )  # Gets percentage composition.
-            table = peptide_table(
+            table: list[tuple[str, str]] = peptide_table(
                 molecular_weight, hydrophobicity
             )  # Creates peptide data table.
             print(figlet.renderText("Results"))  # Displays result title.
@@ -52,11 +52,13 @@ def main() -> None:
                 f"Average Percentage Composition:\n\n{percentage_composition}"
             )  # Displays table.
         elif user_choice == "2":  # Residue analyser route.
-            raw_residue = (
+            raw_residue: str = str(
                 input("\nResidue (For Example, Alanine): ").strip().capitalize()
             )  # Get residue.
-            residue = residue_check(raw_residue)  # Validates residue input.
-            table = residue_table(residue)  # Retrieves residue data.
+            residue: str = residue_check(raw_residue)  # Validates residue input.
+            table: list[tuple[str, str | float]] = residue_table(
+                residue
+            )  # Retrieves residue data.
             print(figlet.renderText("Results"))  # Displays result title.
             print(f"{tabulate(table, tablefmt='double_grid')}")  # Display table.
         else:
@@ -74,7 +76,7 @@ def peptide_check(raw_chain: str) -> str:
         print(
             "\nError: Non-Existent Residue(s) or Invalid Length."
         )  # Prints error message.
-        raw_chain = input("\nInput Chain: ").strip().upper()  # Reprompts user.
+        raw_chain = str(input("\nInput Chain: ")).strip().upper()  # Reprompts user.
 
 
 def calculate_molecular_weight(chain: str) -> float:
@@ -84,31 +86,31 @@ def calculate_molecular_weight(chain: str) -> float:
 
 
 def calculate_hydrophobicity(chain: str) -> float:
-    total_hydrophobicity = sum(
+    total_hydrophobicity: float = sum(
         amino_acid_data[x]["Estimated Average Hydrophobicity"] for x in chain
     )  # Sum hydrophobicity values.
     return total_hydrophobicity / len(chain)  # Returns average.
 
 
 def calculate_percentage_composition(chain: str) -> str:
-    collection = {}  # Counts each residue.
-    length = len(chain)  # Stores chain length.
+    collection: dict[str, int] = {}  # Counts each residue.
+    length: int = len(chain)  # Stores chain length.
     for x in chain:
         if x in collection:
             collection[x] += 1  # Increment if exists.
         else:
             collection[x] = 1  # Initialise if first occurrence.
-    result = []  # Store formatted percentages.
+    result: list[str] = []  # Store formatted percentages.
     for key, value in sorted(collection.items()):
-        percentage = (value / length) * 100  # Calculate percentage.
+        percentage: float = (value / length) * 100  # Calculate percentage.
         result.append(f"{key} | {percentage:.2f}%")  # Format for display.
     return "\n".join(result)  # Combine for output.
 
 
-def peptide_table(molecular_weight: float, hydrophobicity: float) -> list[list[str]]:
-    table = [
-        ["Molecular Weight", f"{molecular_weight} Da"],
-        ["Estimated Hydrophobicity", f"{hydrophobicity:.2f}"],
+def peptide_table(molecular_weight: float, hydrophobicity: float) -> list[tuple[str, str]]:
+    table: list[tuple[str, str]] = [
+        ("Molecular Weight", f"{molecular_weight} Da"),
+        ("Estimated Hydrophobicity", f"{hydrophobicity:.2f}"),
     ]  # Prepare table.
     return table  # Returns table.
 
@@ -119,26 +121,25 @@ def residue_check(raw_residue: str) -> str:
             if re.fullmatch(data["Name"], raw_residue, re.IGNORECASE):
                 return code  # Returns residue if valid.
         print("\nError: Invalid Residue.")  # Prints error message.
-        raw_residue = input(
-            "\nResidue (For Example, Alanine): "
+        raw_residue = str(
+            input("\nResidue (For Example, Alanine): ")
         ).strip()  # Reprompts user.
 
 
-def residue_table(residue: str) -> list[list[str | float]]:
-    residue_info = amino_acid_data[residue]  # Get residue info.
-    table = [
-        ["Residue Name", residue_info["Name"]],
-        ["1-letter Code", residue],
-        ["Molecular Weight", f"{residue_info['Molecular Weight']} Da"],
-        [
+def residue_table(residue: str) -> list[tuple[str, str | float]]:
+    residue_info: dict[str, str | float] = amino_acid_data[residue]  # Get residue info.
+    table: list[tuple[str, str | float]] = [
+        ("Residue Name", residue_info["Name"]),
+        ("1-letter Code", residue),
+        ("Molecular Weight", f"{residue_info['Molecular Weight']} Da"),
+        (
             "Estimated Hydrophobicity",
             residue_info["Estimated Average Hydrophobicity"],
-        ],
-        ["Isoelectric Point", residue_info["pI"]],
+        ),
+        ("Isoelectric Point", residue_info["pI"]),
     ]  # Prepare table.
     return table
 
 
 if __name__ == "__main__":
     main()  # Run main().
-
